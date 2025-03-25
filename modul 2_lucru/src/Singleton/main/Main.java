219e9af1b82d64f5609d38a51d9d5be6ee0902fe
Cosmina <Cosmina@DESKTOP-K1SNVRP>
2025-03-25 06:03:17 +0200

    Singleton

---
+++ modul 2_lucru/src/Singleton/main/Main.java
@@ -0,0 +1,14 @@
+package Singleton.main;
+
+import Singleton.classes.Agentie;
+
+public class Main {
+    public static void main(String[] args) {
+        //cream un ob de tipul clasei
+        Agentie  agentie1=Agentie.getInstance(100,"DerTour");
+        System.out.println(agentie1);
+        Agentie agentie2=Agentie.getInstance(250,"ChristianTour");
+        System.out.println(agentie2);
+
+    }
+}
